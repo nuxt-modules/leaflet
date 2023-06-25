ae50d3ef634b4903878f3c2b81b0ba7a71795707
Gugustinette <mercier.augustin@outlook.fr>
2023-06-25 02:48:56 +0200

    Init Nuxt Leaflet Module

---
+++ nuxt-leaflet/playground/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="height:100vh; width:100vw">
+    <LMap
+      ref="map"
+      :zoom="zoom"
+      :center="[47.21322, -1.559482]"
+    >
+      <LTileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+        layer-type="base"
+        name="OpenStreetMap"
+      />
+    </LMap>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const zoom = ref(6)
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
---
+++ nuxt-leaflet/src/module.ts
@@ -0,0 +1,56 @@
+import { defineNuxtModule, addPlugin, addComponent, createResolver } from '@nuxt/kit'
+
+// Module options TypeScript interface definition
+export interface ModuleOptions {}
+
+// Components to export
+export const components = [
+  'LCircle',
+  'LCircleMarker',
+  'LControl',
+  'LControlAttribution',
+  'LControlLayers',
+  'LControlScale',
+  'LControlZoom',
+  'LFeatureGroup',
+  'LGeoJson',
+  'LIcon',
+  'LImageOverlay',
+  'LLayerGroup',
+  'LMap',
+  'LMarker',
+  'LPolygon',
+  'LPolyline',
+  'LPopup',
+  'LRectangle',
+  'LTileLayer',
+  'LTooltip',
+  'LWmsTileLayer'
+]
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'nuxt-leaflet',
+    configKey: 'nuxtLeaflet',
+    compatibility: {
+      nuxt: '^3.0.0'
+    }
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  async setup (options, nuxt) {
+    // Add Leaflet's CSS
+    nuxt.options.css.push('leaflet/dist/leaflet.css')
+
+    // Export Vue Leaflet components
+    for (const component of components) {
+      addComponent({
+        name: component,
+        export: component,
+        filePath: '@vue-leaflet/vue-leaflet',
+        chunkName: `nuxt-leaflet/${component}`,
+        mode: 'all'
+      })
+    }
+  }
+})
