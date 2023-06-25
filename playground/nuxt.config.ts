08372119b1d58ac358c847da5be82c275f4d0a25
Gugustinette <mercier.augustin@outlook.fr>
2023-06-25 04:26:57 +0200

    Need to import leaflet from CDN... Yes.......

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,17 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   myModule: {},
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      script: [
+        // Leaflet's JS
+        {
+          src: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+          integrity: 'sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=',
+          crossorigin: ''
+        }
+      ]
+    }
+  } 
 })
