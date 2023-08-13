00bf5afc43920b1c622e1338c0c10c818761c58f
Gugustinette <mercier.augustin@outlook.fr>
2023-08-13 16:57:34 +0200

    Try to deploy Playground to Github Pages (SSG)

--- playground/app.vue
+++ playground/app.vue
@@ -1,40 +1,17 @@
 <template>
-  <div style="height:100vh; width:100vw">
-    <div>
-      <h1>Hello Nuxt</h1>
+  <div>
+    <nav>
       <ul>
-        <li>Element 1</li>
-        <li>Element 2</li>
-        <li>Element 3</li>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/map/search">Map</nuxt-link></li>
       </ul>
-      <p>
-        Theses components should be rendered on the server, so they're received on the client as HTML.
-        Only the map below should be rendered on the client.
-      </p>
-    </div>
-    <LMap
-      ref="map"
-      :zoom="zoom"
-      :center="[47.21322, -1.559482]"
-    >
-      <LTileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-        layer-type="base"
-        name="OpenStreetMap"
-      />
-    </LMap>
+    </nav>
+    <NuxtPage />
   </div>
 </template>
-
-<script setup>
-import { ref } from 'vue'
-const zoom = ref(6)
-</script>
 
 <style>
 body {
   margin: 0;
-  font-family: sans-serif;
 }
 </style>
---
+++ playground/pages/map/search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="height:100vh; width:100vw">
+    <h1>Hello Map</h1>
+    <ul>
+      <li>Element 1</li>
+      <li>Element 2</li>
+      <li>Element 3</li>
+      <li>Element 4</li>
+    </ul>
+    <p>This is a paragraph</p>
+    <LMap
+      ref="map"
+      :zoom="zoom"
+      :center="[47.21322, -1.559482]"
+    >
+      <LTileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+        layer-type="base"
+        name="OpenStreetMap"
+      />
+    </LMap>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const zoom = ref(6)
+</script>
