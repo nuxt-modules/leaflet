f424760284edd0b3f7b6461ed61a6adf514ba419
Gugustinette <mercier.augustin@outlook.fr>
2023-08-13 17:31:59 +0200

    Fix CI URL ?

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,6 +2,6 @@ export default defineNuxtConfig({
   modules: ['../src/module'],
   devtools: { enabled: true },
   app: {
-    buildAssetsDir: '/Nuxt-Leaflet/',
-  },
+    baseURL: '/Nuxt-Leaflet/'
+  }
 })
