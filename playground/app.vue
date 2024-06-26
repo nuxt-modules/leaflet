bdb81724ca72fe3b59dbb3d45f05b0dab4a14710
Gugustinette <mercier.augustin@outlook.fr>
2024-06-26 16:19:42 +0200

    Add Nuxt UI to improve playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,31 +1,38 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Home</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/map/map">Map</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/map/geojson">GeoJson</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/map/markercluster">Marker Cluster</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/map/draw">Draw</NuxtLink>
-        </li>
-      </ul>
-    </nav>
+  <div id="app">
+    <UVerticalNavigation :links="links" />
     <NuxtPage />
   </div>
 </template>
 
+<script setup lang="ts">
+const links = [{
+  label: 'Home',
+  icon: 'i-heroicons-home',
+  to: '/'
+}, {
+  label: 'Map',
+  icon: 'i-heroicons-map-solid',
+  to: '/map/map'
+}, {
+  label: 'GeoJson',
+  icon: 'i-heroicons-map-solid',
+  to: '/map/geojson'
+}, {
+  label: 'MarkerCluster',
+  icon: 'i-heroicons-rectangle-group-16-solid',
+  to: '/map/markercluster'
+}, {
+  label: 'Draw (Deprecated)',
+  icon: 'i-heroicons-pencil-16-solid',
+  to: '/map/draw'
+}]
+</script>
+
 <style>
 body {
   margin: 0;
+  padding: 0;
   background-color: #020420;
   color: #fff;
   font-family: 'Arial', sans-serif;
@@ -33,5 +40,11 @@ body {
 
 a {
   color: #00DC82;
+}
+
+#app {
+  display: flex;
+  flex-direction: row;
+  height: 100svh;
 }
 </style>
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: ['../src/module', '@nuxt/ui'],
   devtools: {
     enabled: true,
 
