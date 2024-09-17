81fea2f579f0bb03f517291b3c542e82d9f18a2d
Gugustinette <mercier.augustin@outlook.fr>
2024-09-17 13:58:49 +0200

    test: disable ssr

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -1,8 +1,7 @@
 import NuxtLeaflet from '../../../src/module'
 
 export default defineNuxtConfig({
-  modules: [
-    NuxtLeaflet,
-  ],
+  modules: [NuxtLeaflet],
   compatibilityDate: '2024-04-03',
+  ssr: false,
 })
